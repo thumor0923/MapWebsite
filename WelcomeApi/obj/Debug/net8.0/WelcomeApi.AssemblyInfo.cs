e586176928180eb95cecffab94a621012a179ae7
LAPTOP-FFBRAO80\user <yuuki.ework@gmail.com>
2025-03-17 14:25:09 +0800

    daily

--- WelcomeApi/obj/Debug/net8.0/WelcomeApi.AssemblyInfo.cs
+++ WelcomeApi/obj/Debug/net8.0/WelcomeApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WelcomeApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1f573fc8d1dab06fbdc06647bdad2fc5500d255")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb96b1b808c6e3825c30f156aba93279fcaec183")]
 [assembly: System.Reflection.AssemblyProductAttribute("WelcomeApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WelcomeApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
